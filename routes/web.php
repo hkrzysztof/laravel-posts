8851c64000d3dc6dd78f85ef2b9db33d26cbbf56
Krzysztof <krzysztof.hajduk@sarigato.com>
2021-01-31 12:55:45 +0100

    added tailwind css, tested out db

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/register');
+
+Route::get('/posts', function () {
+    return view('posts.index');
 });
